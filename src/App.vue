f270d506e3c48c792d87df776175ba6547112c3d
marciaalegria <ytasegundo@gmail.com>
2020-09-27 21:47:36 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="titulo" id="app">
+            <hijo></hijo>
+        </div>
+        <div class="formulario">
+  
+        </div>
+        <div class="result">
+            
+        </div>
+    </div>
+
+</template>
+
+<script>
+import Hijo from "./components/Hijo.vue";
+  export default {
+    data() {
+      return {
+        message: 'Random Gif-Cat',
+      };
+    },
+    components:{
+        'hijo': Hijo,
+    }
+  }
+</script>
+
+<style scoped>
+  .titulo {
+    font-family: "Calibri", sans-serif;
+    color: darkblue;
+    background: lightblue;
+  }
+  .formulario{
+    background: rgb(241, 139, 144);
+  }
+  .result{
+    background: lightblue;
+  }
+</style>
---
+++ src/components/Hijo.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+    
+    <h1>Random Gif Cat</h1>
+    <form action="">
+
+        <label for="">Título</label>
+        <input type="text" v-model="gatitos.titulo">
+        <label for="">Filtro</label>
+        <select name="" id=""></select>    
+        <label for="">Color</label>
+        <select name="" id=""></select>
+        <label for="">Tamaño</label>
+        <input type="text">
+        <input type="submit" value="Obtener mi gatito" @click.prevent="fetchCharacter">
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'hijo-component',
+    // props: {},
+    data: function(){
+        return {
+            gatitos: {
+                titulo:'',
+                filtro:'',
+                color:'',
+                tamaño:'',
+            }
+        }
+    },
+    computed: {
+        image(){
+            return this.gatitos.image;
+        }
+    },
+    methods: {
+        fetchCharacter(){
+            //fetch(`https://cataas.com/cat/gif/says/${this.gatitos.titulo}?filter=${this.gatitos.filtro}&color=${this.gatitos.color}&size=${this.gatitos.tamaño}&type=or`)
+            fetch(`https://cataas.com/cat/gif/says/holi?filter=sepia&color=orange&size=40&type=or`)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+                this.addCharacter(json);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        },
+        addCharacter(json){
+            if(json.results[0] === undefined){
+                alert('no ta');
+                return;
+            }
+            this.gatitos = json.results[0];
+        }
+    },
+    // components: {},
+}
+</script>
+
+<style scoped>
+    
+</style>
